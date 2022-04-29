4f54f5ab7681d2474be27968865694994c0193d7
Oscar Barajas Tavares <oscar@gndx.dev>
2022-04-29 16:06:31 -0500

    :memo: [ADD] asignacion / declaracion

---
+++ src/scope/re.js
@@ -0,0 +1,32 @@
+var firstName; // Undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = "David"; // declarar / asignar
+lastName = 'Ana'; // reasignar
+console.log(lastName); 
+
+var secondName = 'David'; // declarando / asignando
+var secondName = 'Ana'; // reasignado
+console.log(secondName);
+
+// Let
+let fruit = 'Apple' // declara y asignar
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+
+let fruit = 'Banana';
+console.log(fruit);
+
+// const
+const animal = 'dog'; // declara y asigna
+// animal = 'cat'; // reasignado
+const animal = 'Snake';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("🚗");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
