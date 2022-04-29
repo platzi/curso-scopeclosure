210712764c77b36c32a49fef9dbb8e0cc80c5485
Oscar Barajas Tavares <oscar@gndx.dev>
2022-04-29 15:35:01 -0500

    :memo: [ADD] Function scope

---
+++ src/scope/function.js
@@ -0,0 +1,12 @@
+function greeting() {
+  let userName = 'Ana'; 
+  console.log(userName);
+
+  if (userName === 'Ana') {
+    console.log(`Hello ${userName}!`)
+  }
+
+}
+
+greeting();
+console.log(userName);
